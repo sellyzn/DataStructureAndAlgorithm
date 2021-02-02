b0f253950e471df99ba3a9dc0ee24895d32b52eb
Ning Zhang <sellyzhang@outlook.com>
2021-02-01 23:03:04 -0800

    Add LinkedList

--- LeetCodeLabuladong/LeetCodeLabuladong/Program.cs
+++ LeetCodeLabuladong/LeetCodeLabuladong/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCodeLabuladong.BinaryTree;
+using System;
 
 namespace LeetCodeLabuladong
 {
@@ -7,6 +8,44 @@ namespace LeetCodeLabuladong
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public int KthLargest(TreeNode root, int k)
+        {
+            this.k = k;
+            dfs(root);
+            return res;
+        }
+        int k;
+        int res;
+        public void dfs(TreeNode root)
+        {
+            if (root == null)
+                return;
+            dfs(root.right);
+            if (k == 0)
+                return;
+            if (--k == 0)
+                res = root.val;
+            dfs(root.left);
+        }
+
+        //先序遍历树A中的每个节点nA
+        public bool IsSubStructure(TreeNode A, TreeNode B)
+        {
+            if (A == null || B == null)
+                return false;
+            //if (A.val == B.val && Recursive(A.left, B.left) && Recursive(A.right, B.right))
+            //    return true;
+            return Recursive(A, B) || IsSubStructure(A.left, B) || IsSubStructure(A.right, B);
+        }
+        //判断树A中以nA为根节点的子树是否包含树B
+        public bool Recursive(TreeNode A, TreeNode B)
+        {
+            if (B == null)
+                return true;
+            if (A == null || A.val != B.val)
+                return false;
+            return Recursive(A.left, B.left) && Recursive(A.right, B.right);            
         }
     }
 }
